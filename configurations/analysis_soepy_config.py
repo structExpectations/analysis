b34133f53af70e3a709467eeb6689b14adffe87c
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-12-06 10:57:28 +0100

    Added configuration file with paths for important directories.

---
+++ configurations/analysis_soepy_config.py
@@ -0,0 +1,15 @@
+"""This module provides some configuration for the package."""
+import sys
+import os
+from pathlib import Path
+
+import numpy as np
+
+# We only support modern Python.
+np.testing.assert_equal(sys.version_info[:2] >= (3, 6), True)
+
+# We rely on relative paths throughout the package.
+PACKAGE_DIR = Path(os.path.abspath(__file__)).parents[1]
+RESOURCES_DIR = PACKAGE_DIR / "estimation" / "basecamp" / "resources"
+LOGGING_DIR = PACKAGE_DIR / "logging"
+FIGURES_DIR = PACKAGE_DIR / "figures"
