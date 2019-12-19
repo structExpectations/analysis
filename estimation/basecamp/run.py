b29a295dfaed501ee6e099ea6113706dc8f21436
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-12-19 17:15:23 +0100

    Bounds included in init file. Changes logging and figures location.

--- estimation/basecamp/run.py
+++ estimation/basecamp/run.py
@@ -1,72 +1,18 @@
 #!usr/bin/env python
 import pickle
 
-import numpy as np
+import pandas as pd
 
 from estimagic.optimization.optimize import minimize
-from python.auxiliary import prepare_estimation
 from estimation.basecamp.moments_calculation import get_moments
 from python.SimulationBasedEstimation import SimulationBasedEstimationCls
-
-
-lower = np.tile(
-    (
-        1.000,
-        1.000,
-        1.000,
-        0.050,
-        0.050,
-        0.050,
-        0.005,
-        0.005,
-        0.005,
-        0.001,
-        0.001,
-        0.001,
-        1.00,
-        1.00,
-        -0.400,
-        -0.800,
-        0.05,
-        0.001,
-        0.001,
-        0.001,
-    ),
-    1,
-)
-
-upper = np.tile(
-    (
-        3.000,
-        3.000,
-        3.000,
-        0.400,
-        0.400,
-        0.400,
-        0.600,
-        0.600,
-        0.600,
-        0.150,
-        0.150,
-        0.150,
-        4.00,
-        4.00,
-        -0.050,
-        -0.150,
-        0.500,
-        0.800,
-        0.800,
-        0.800,
-    ),
-    1,
-)
 
 model_params_init_file_name = "resources/toy_model_init_file_03_2types.pkl"
 model_spec_init_file_name = "resources/model_spec_init.yml"
 log_file_name_extension = "test"
 
 
-model_params_df = prepare_estimation(model_params_init_file_name, lower, upper)
+model_params_df = pd.read_pickle(model_params_init_file_name)
 
 with open("resources/moments_obs.pkl", "rb") as f:
     moments_obs = pickle.load(f)
