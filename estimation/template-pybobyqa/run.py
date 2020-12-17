7a9725a218cf8014b5735b8976c87fa2cf0f8c9d
peisenha <peisenha@protonmail.com>
2020-12-17 16:12:39 +0100

    added pybobyca template

--- estimation/template-pybobyqa/run.py
+++ estimation/template-pybobyqa/run.py
@@ -2,12 +2,13 @@
 from functools import partial
 import pickle
 
-from scipy import optimize as opt
+import pybobyqa as pybob
 import pandas as pd
+import numpy as np
 
 from SimulationBasedEstimation import SimulationBasedEstimationCls
 
-from global_auxiliary import prepare_bounds
+from global_auxiliary import prepare_optimizer_interface
 from global_auxiliary import wrapper_numpy
 from global_moments import get_moments
 
@@ -25,8 +26,10 @@ model_params.loc[("const_wage_eq", "gamma_0s1"), "fixed"] = False
 
 # We set the tuning parameters of the optimizer so that it runs forever.
 opt_kwargs = dict()
-opt_kwargs["maxiter"] = 1000000
-opt_kwargs["tol"] = 0.0
+opt_kwargs["scaling_within_bounds"] = True
+opt_kwargs["seek_global_minimum"] = True
+opt_kwargs["objfun_has_noise"] = True
+opt_kwargs["maxfun"] = 1
 
 # We need to set up our criterion function.
 adapter_kwargs = dict()
@@ -34,12 +37,12 @@ adapter_kwargs["model_spec_init_file_name"] = model_spec_fname
 adapter_kwargs["weighting_matrix"] = weighting_matrix
 adapter_kwargs["moments_obs"] = moments_obs
 adapter_kwargs["get_moments"] = get_moments
-
 adapter_kwargs["params"] = model_params
 
 adapter_smm = SimulationBasedEstimationCls(**adapter_kwargs)
 
 # Ready for the optimization.
-model_params, bounds = prepare_bounds(model_params)
+x0, bounds = prepare_optimizer_interface(model_params)
 p_wrapper_numpy = partial(wrapper_numpy, model_params, adapter_smm)
-opt.differential_evolution(p_wrapper_numpy, bounds, **opt_kwargs)
+rslt = pybob.solve(p_wrapper_numpy, x0, bounds=bounds, **opt_kwargs)
+np.testing.assert_almost_equal(rslt.f, 4642.07887028819)
